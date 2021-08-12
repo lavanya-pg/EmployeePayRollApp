e1621d88279e652df3f1f77948e91a8bc7001ce8
lavanya-pg <lavanyapg8@gmail.com>
2021-08-12 21:56:57 +0530

    Added: Ability to view Employee Payroll Details From Local Storage

--- Js/Home.js
+++ Js/Home.js
@@ -1,10 +1,16 @@
+let employeePayrollList;
 window.addEventListener("DOMContentLoaded", (event) => {
+    employeePayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
     createInnerHtml();
 });
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
 const createInnerHtml = () => {
     const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>"
+    if (employeePayrollList.length == 0) return;
     let innerHtml = `${headerHtml}`;
-    let employeePayrollList = createEmployeePayrollJSON();
     for (const employeePayrollData of employeePayrollList) {
         innerHtml = `${innerHtml}
         <tr>
@@ -15,8 +21,8 @@ const createInnerHtml = () => {
             <td>${employeePayrollData._salary}</td>
             <td>${employeePayrollData._startDate}</td>
             <td>
-                <img name="${employeePayrollData._id}" onclick="remove(this)" alt="delete" src="C:\Users\Lavanya\Desktop\EmployeePayrollPay\Asserts\Icons\delete-black-18dp.svg">
-                <img name="${employeePayrollData._id}" alt="edit" onclick="update(this)" src="C:\Users\Lavanya\Desktop\EmployeePayrollPay\Asserts\Icons\create-black-18dp.svg">
+                <img id="${employeePayrollData._id}" onclick="remove(this)" alt="delete" src="C:\Users\Lavanya\Desktop\EmployeePayrollPay\Asserts\Icons\delete-black-18dp.svg">
+                <img id="${employeePayrollData._id}" alt="edit" onclick="update(this)" src="C:\Users\Lavanya\Desktop\EmployeePayrollPay\Asserts\Icons\create-black-18dp.svg">
             </td>
         </tr>
         `;
@@ -28,21 +34,21 @@ const createEmployeePayrollJSON = () => {
             _id: new Date().getTime(),
             _name: "Lavanya",
             _salary: "$ 100000",
-            _gender: "Female",
-            _department: [""],
-            _notes: "Good Employee",
-            _profile: "C:\Users\Lavanya\Desktop\EmployeePayrollPay\Asserts\Profile-Images\Ellipse -3.png",
+            _gender: "female",
+            _department: ["Engineering"],
+            _notes: "Nice",
+            _profile: "C:\Users\Lavanya\Desktop\EmployeePayrollPay\Asserts\Profile-Images\Ellipse -1.png",
             _startDate: "18/09/2020, 12:00:00 AM"
         },
         {
             _id: new Date().getTime() + 1,
             _name: "Sandhiya",
             _salary: "$ 70000",
-            _gender: "Female",
+            _gender: "female",
             _department: ["Engineering", "Sales"],
-            _notes: null,
-            _profile: "C:\Users\Lavanya\Desktop\EmployeePayrollPay\Asserts\Profile-Images\Ellipse -8.png",
-            _startDate: "18/09/2020, 12:00:00 AM"
+            _notes: "null",
+            _profile: "C:\Users\Lavanya\Desktop\EmployeePayrollPay\Asserts\Profile-Images\Ellipse -7.png",
+            _startDate: "8/12/2019, 12:00:00 AM"
         }
     ];
     return employeePayrollListLocal;
@@ -53,4 +59,16 @@ const getDeptHtml = (deptList) => {
         deptHtml = `${deptHtml} <div class="dept-label">${dept}</div>`
     }
     return deptHtml;
+}
+function remove(node){
+    let empPayrollData = employeePayrollList.find(empData => empData._id == node.id);
+    if (!empPayrollData) return;
+    const index = employeePayrollList
+        .map(empData => empData._id)
+        .indexOf(empPayrollData._id);
+    employeePayrollList.splice(index, 1);
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
+    document.querySelector('.emp-count').textContent = employeePayrollList.length;
+    alert("User deleted is : "+empPayrollData._name+"with id :"+empPayrollData._id);
+    createInnerHtml();
 }
